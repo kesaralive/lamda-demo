2a6ec2d3edf134ed6a3406544046718951462901
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-04-12 08:06:07 +0530

    feat: add numbers & specials to lambda

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 def lambda_handler(event, context):
     alphabet = {'lower': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-                'upper': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'], }
+                'upper': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+                'numbers': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                'special': ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', '?', '/', '.', ',', ' ']}
     df = pd.DataFrame(data=alphabet)
     print(df)
     print('Done!')
